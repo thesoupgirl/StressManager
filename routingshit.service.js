86751b0e53b9ecf4b720705763b5fa9440389a49
patriciachunk <chuny@purdue.edu>
2016-02-20 01:30:53 -0600

    added some

--- routingshit.service.js
+++ routingshit.service.js
@@ -1,16 +1,20 @@
+
 var stressApp = angular.module('stressManagerApp', []);
   
 stressApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
+
       when('/addGoogleCalendar', {
         templateUrl: 'templates/add-google-calendar.html',
         controller: 'AddGoogleCalendarController'
       }).
+
       when('/showGoogleCalendar', {
         templateUrl: 'templates/show-google-calendar.html',
         controller: 'ShowGoogleCalendarController'
       }).
+      
       otherwise({
         redirectTo: '/addUser'
       });
