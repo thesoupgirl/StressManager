25d47802f64081f5ccb6974bdc881f95646dfc81
Lisa Campbell <campb215@purdue.edu>
2016-02-20 11:06:27 -0600

    Fixed shit

--- calc.controller.js
+++ calc.controller.js
@@ -18,7 +18,7 @@ angular.module('stressApp.controllers').controller('Calc', ['$scope', '$http', f
       $scope.getVal=function(){
 	        console.log($scope.radio);
 	        $scope.change=$scope.radio;
-    	}
+    	};
 
 		$scope.isStudent = function(){
 			$scope.isStudent = true;
